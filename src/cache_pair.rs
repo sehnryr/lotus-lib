15438c209efec8ae54b251c46b9281e14c1b37f6
sehnryr <youn@melois.dev>
2023-01-25 16:22:30 +0100

    add cache_pair trait

---
+++ src/cache_pair.rs
@@ -0,0 +1,15 @@
+pub trait CachePair {
+    const MAGIC_NUMBER: u64 = 0x1867C64E;
+    const ARCHIVE_VERSION: u64 = 20;
+
+    fn new(
+        toc_path: Option<String>,
+        cache_path: Option<String>,
+        is_post_ensmallening: bool,
+    ) -> Self;
+    fn is_post_ensmallening(&self) -> bool;
+    fn toc_path(&self) -> String;
+    fn cache_path(&self) -> String;
+    fn read_toc(&mut self);
+    fn unread_toc(&mut self);
+}
