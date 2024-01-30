6861cf7443d8bf7bbc5d6a1d85050e758bacd196
sehnryr <youn@melois.dev>
2024-01-31 00:38:36 +0100

    perf: :zap: improve entry name parsing
    We can assume the entry name is utf8, so it's sensible to use `from_utf8` instead of its `lossy` counterpart which checks internally for non-utf8 characters

--- src/toc/toc.rs
+++ src/toc/toc.rs
@@ -71,11 +71,10 @@ impl Toc {
         for entry in entries {
             // Entry name is a null-terminated string, so we need to find the
             // index of the null byte and truncate the string there
-            let entry_name = match entry.name.iter().position(|&x| x == 0) {
-                Some(index) => String::from_utf8_lossy(&entry.name[0..index]),
-                _ => String::from_utf8_lossy(&entry.name),
-            }
-            .into_owned();
+            let entry_name: String = match entry.name.iter().position(|&x| x == 0) {
+                Some(index) => std::str::from_utf8(&entry.name[0..index]),
+                _ => std::str::from_utf8(&entry.name),
+            }?.to_string();
 
             let parent_node = match self.directories.get(entry.parent_dir_index as usize) {
                 Some(parent_node) => parent_node.clone(),
