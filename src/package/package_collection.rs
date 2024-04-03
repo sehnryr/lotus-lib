f9d102d1108f6bd85d457058b1f1fdc065d4fa72
sehnryr <youn@melois.dev>
2024-04-03 16:05:07 +0200

    feat: wrap return type with Result for when the directory doesn't exist

--- src/package/package_collection.rs
+++ src/package/package_collection.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::path::PathBuf;
 
 use crate::cache_pair::CachePair;
@@ -10,25 +11,24 @@ pub struct PackageCollection<T: CachePair> {
 }
 
 impl<T: CachePair> PackageCollection<T> {
-    pub fn new(directory: PathBuf, is_post_ensmallening: bool) -> Self {
-        let mut package_collection = Self {
-            directory,
-            is_post_ensmallening,
-            packages: Vec::new(),
-        };
-        package_collection.load_packages();
-        package_collection
-    }
+    /// Creates a new package collection from the specified directory.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the directory does not exist or if the directory cannot be read.
+    pub fn new<P>(directory: P, is_post_ensmallening: bool) -> Result<Self, io::Error>
+    where
+        P: Into<PathBuf>,
+    {
+        let directory = directory.into();
 
-    fn load_packages(&mut self) {
-        let package_directory = self.directory.clone();
-        for entry in std::fs::read_dir(package_directory.clone()).unwrap() {
-            let entry = entry.unwrap();
+        let mut packages = Vec::new();
+        for entry in std::fs::read_dir(&directory).unwrap() {
+            let entry = entry?;
             let file_name = entry.file_name().into_string().unwrap();
 
-            // Check if the file has enough characters to be a package
-            // 7 characters counts for the shortest possible package name of
-            // 1 character : H.1.toc
+            // Check if the file has enough characters to be a package 7 characters counts for the
+            // shortest possible package name of 1 character : H.1.toc
             if file_name.len() < 7 {
                 continue;
             }
@@ -40,13 +40,15 @@ impl<T: CachePair> PackageCollection<T> {
 
             let package_name = file_name[2..file_name.len() - 4].to_string();
 
-            let package = Package::new(
-                package_directory.clone(),
-                package_name,
-                self.is_post_ensmallening,
-            );
-            self.packages.push(package);
+            let package = Package::<T>::new(&directory, package_name, is_post_ensmallening);
+            packages.push(package);
         }
+
+        Ok(Self {
+            directory,
+            is_post_ensmallening,
+            packages,
+        })
     }
 
     pub fn is_post_ensmallening(&self) -> bool {
